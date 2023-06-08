85e4d574c97e90bcc954f4597ae1a93c89d4530e
Yoda-Flash <yaddle.yoda.flash@gmail.com>
2023-06-07 23:24:06 -0700

    Finished

---
+++ BaseProgressBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="flex h-2 border border-grey-30 rounded">
+      <div
+        :style="{ width: `${width}%` }"
+        :class="[color]"
+        class="progress-bar flex h-full"
+      />
+    </div>
+  </template>
+  
+  <script>
+  import { computed } from 'vue';
+  
+  export default {
+    props: {
+      max: {
+        type: Number,
+        required: true,
+      },
+  
+      value: {
+        type: Number,
+        required: true,
+      },
+  
+      color: {
+        type: String,
+        default: 'bg-transparent',
+      },
+    },
+  
+    setup(props) {
+      const width = computed(() => (props.value / props.max) * 100);
+  
+      return { width };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .progress-bar {
+    transition: 0.5s ease-in-out;
+  }
+  </style>
+  
---
+++ PasswordInputForm.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="container mx-auto w-64">
+      <h1 class="text-xl text-center">Password strength checker</h1>
+      <input
+        class="
+          block
+          w-full
+          my-1
+          p-2
+          border border-grey-30
+          rounded
+          leading-normal
+          text-grey-80
+        "
+        type="text"
+        v-model="password"
+      />
+  
+      <PasswordScore
+        :value="password"
+        class="mt-2 mb-6"
+        @passed="isPasswordStrong = true"
+        @failed="isPasswordStrong = false"
+      />
+  
+      <button
+        class="
+          mt-6
+          px-6
+          py-1
+          leading-lg
+          select-none
+          bg-blue-400
+          text-white
+          font-semibold
+          border border-blue-500
+          rounded
+        "
+        :class="[
+          {
+            'cursor-not-allowed opacity-50 pointer-events-hover':
+              !isPasswordStrong,
+          },
+        ]"
+      >
+        Submit
+      </button>
+    </div>
+  </template>
+  
+  <script>
+  import { ref } from 'vue';
+  import PasswordScore from './PasswordScore.vue';
+  
+  export default {
+    components: {
+      PasswordScore,
+    },
+  
+    setup() {
+      const password = ref('');
+      const isPasswordStrong = ref(false);
+  
+      return { password, isPasswordStrong };
+    },
+  };
+  </script>
+  
